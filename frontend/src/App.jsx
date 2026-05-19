4993ad8d028f83da1ea3a4877774fb773b6f238d
Gaurav <gauravlekhwar7404@gmail.com>
2026-05-19 14:55:43 +0530

    started working on frontend, added index.html and main.jsx files, set up basic React app structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from 'axios';
+import QRCode from "react-qr-code"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+
+function app(){
+  const [url , setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+  const[copied, setCopied] = useState("false");
+  const [qrImage, setQrImage] = useState("")
+
+  const handleShorten = async () =>{
+    if(!url) return;
+
+    try{
+      const res = await axios.post(`${API_BASE_URL}/shorten`,{
+        originalUrl:url
+      });
+
+      const newshortUrl = res.data.shortUrl;
+      setShortUrl(newshortUrl);
+      setCopied(false);
+
+
+
+
+
+    } catch(err){
+      console.log(err);
+      alert("something went wrong")
+    }
+  }
+
+
+
+
+  return <div>MERN URL SHORTENER</div>
 }
 
-export default App
+export default App;
